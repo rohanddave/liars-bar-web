696ee27d16f754121e6162038dd3708cf4f47bd4
Rohan Dave <rohandewangdave@gmail.com>
2025-08-25 18:15:58 -0700

    setup room dtos

--- liars-bar-backend/src/rooms/controller/rooms.controller.ts
+++ liars-bar-backend/src/rooms/controller/rooms.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Post, Param, Body } from '@nestjs/common';
 import { RoomsService } from '../service/rooms.service';
+import { CreateRoomDto } from '../dto/create-room.dto';
 
 @Controller('room')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
   @Post()
-  createRoom(@Body() body: any) {
+  createRoom(@Body() body: CreateRoomDto) {
     return '';
   }
 
